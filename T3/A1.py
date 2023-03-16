e317f97562f84c9d9b699f52eaeabdcadbbde71f
ArathHernandez <47037125+ArathHernandez@users.noreply.github.com>
2023-03-16 15:02:02 -0600

    Update A1.py

--- T3/A1.py
+++ T3/A1.py
@@ -1,9 +1,10 @@
 import cv2
 import matplotlib.pyplot as plt
 
-imagen = cv2.imread('Numeros_Binarizacion.jpg', cv2.IMREAD_GRAYSCALE)
+imagen = cv2.imread('sudoku.jpg', cv2.IMREAD_GRAYSCALE)
+imagen = cv2.resize(imagen, (800, 800))
 
-cv2.imshow('Imagen en blanco y negro', imagen)
+cv2.imshow('Imagen B/N', imagen)
 
 hist = cv2.calcHist([imagen], [0], None, [256], [0, 256])
 
