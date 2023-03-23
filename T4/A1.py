cbc74e41b882b53fff8fc727025e944eb958361f
ArathHernandez <47037125+ArathHernandez@users.noreply.github.com>
2023-03-23 16:25:53 -0600

    Update A1.py

--- T4/A1.py
+++ T4/A1.py
@@ -2,25 +2,16 @@ import cv2
 import numpy as np
 from scipy import ndimage
 
-# Cargar la imagen
-img = cv2.imread('pasaporte.jpg', cv2.IMREAD_GRAYSCALE)
-img = cv2.resize(img, (700, 700))
+imagen = cv2.imread('Hipo.jpg', cv2.IMREAD_GRAYSCALE)
+imagen = cv2.resize(imagen, (700, 700))
 
-# Definir el kernel de Sobel en dirección X
-kernel = np.array([[-1, 0, 1],
-                   [-2, 0, 2],
-                   [-1, 0, 1]])
+kernel = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
 
-# Realizar la convolución utilizando la función filter2D de OpenCV
-dst = cv2.filter2D(img, -1, kernel)
+imagen_convolve = ndimage.convolve(imagen, kernel)
+magnitud = np.abs(imagen_convolve)
+escalada = magnitud * 255.0 / np.max(magnitud)
+sobel_x = np.uint8(escalada)
 
-# Calcular la magnitud de la derivada
-dst = np.abs(dst)
-
-# Escalar los valores al rango [0, 255]
-dst = (dst * 255 / np.max(dst)).astype(np.uint8)
-
-# Mostrar la imagen resultante
-cv2.imshow('Derivada de Sobel en direccion X', dst)
+cv2.imshow('Sobel en X', sobel_x)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
